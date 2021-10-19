1e2b7dec8c89a85dbeed178ec3cbc6db3635ca29
sinenhlanhla magubane <snemagubane06@gmail.com>
2021-10-19 21:16:41 +0200

    task 1.4 commit

---
+++ task1.4.js
@@ -0,0 +1,11 @@
+function drawSquare(num) {
+  let string = '';
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num; j++) {
+      string += ' # ';
+    }
+    string += '\n';
+  }
+  console.log(string);
+}
+drawSquare(3);
